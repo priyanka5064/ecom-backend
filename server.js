5cd34213a10cd075f29781b2566ca167656496db
klebcabailhongal <gundennavarrakshita@gmail.com>
2025-07-25 15:43:13 +0530

    Authentication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/", productRoutes);
+
+app.use("/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL)
